3866213a32425430ca0c64a4e1527ca830aa65f7
Eyal85 <eyal038020897@gmail.com>
2023-05-06 22:35:51 +0300

    roller coster

---
+++ roller coster limit/js/main.js
@@ -0,0 +1,9 @@
+var height= prompt("please enter your height");
+var accses= false;
+var limit = 150;
+if(height>limit){
+    accses=true
+    alert("you can enter")
+}
+else{
+    alert("you cannot enter")}
